b96c56e4a503feef2928f3457a1920d620cd258f
malbasini <malbasini@gmail.com>
2021-12-10 16:23:08 +0100

    Commit

--- Colori/script.js
+++ Colori/script.js
@@ -5,10 +5,8 @@ let blue = document.getElementsByClassName("blue");
 let alpha = document.getElementsByClassName("alpha");
 function changeRGB()
 {
-    let r = red[0].value;
-    let g = green[0].value;
-    let b = blue[0].value;
-    let a = alpha[0].value/10;
+    let r = red[0].value; let g = green[0].value;
+    let b = blue[0].value; let a = alpha[0].value/10;
     myDiv[0].style.background = "rgba("+r+","+g+","+b+","+a+")";
     myDiv[0].innerHTML = "<h2> COLORE: rgba("+r+","+g+","+b+","+a+") </h2>";
 }
